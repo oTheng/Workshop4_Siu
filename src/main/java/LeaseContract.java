13478fc95736d3d6f41d59c865a6d51352c2c8b6
Tsiu11 <Tsiu11@wgu.edu>
2026-05-15 11:43:27 -0400

    Switch SalesContract signature and signature LeaseContract(It was opposite)

--- src/main/java/LeaseContract.java
+++ src/main/java/LeaseContract.java
@@ -5,45 +5,14 @@ public class LeaseContract extends Contract {
     }
 
     @Override
-    double getTotalPrice(Vehicle vehicle, boolean finance, double loan) {
-        double saleTax = vehicle.getPrice() * 0.05;
-        double recordingFee = 100;
-        double processingFee;
-        double finalprice = 0;
-        if (vehicle.getPrice() <= 10000) {
-            processingFee = 295;
-        } else {
-            processingFee = 495;
-        }
-        if (finance) {
-            finalprice = getMonthlyPayment(vehicle, loan);
-        } else {
-            finalprice = vehicle.getPrice() + saleTax + recordingFee + processingFee;
-        }
-        return finalprice;
+    double getTotalPrice(Vehicle vehicle, boolean finance, double loan){
+
+        return loan;
     }
 
     @Override
     double getMonthlyPayment(Vehicle vehicle, double loan) {
-        double monthlyCalculation = 0;
-        if (loan < vehicle.getPrice()) {
-            System.out.println("You need to take out a bigger loan!");
-        } else {
-            double annualInterestRate;
-            int months;
-            if (loan >= 10000) {
-                annualInterestRate = 0.0425;
-                months = 48;
-            } else {
-                annualInterestRate = 0.0525;
-                months = 24;
-            }
 
-            double monthlyRate = annualInterestRate / 12;
-
-            monthlyCalculation = loan * (monthlyRate * Math.pow(1 + monthlyRate, months)) / (Math.pow(1 + monthlyRate, months) - 1);
-        }
-
-        return monthlyCalculation;
+        return loan;
     }
 }
--- src/main/java/SalesContract.java
+++ src/main/java/SalesContract.java
@@ -6,13 +6,44 @@ public class SalesContract extends Contract{
 
     @Override
     double getTotalPrice(Vehicle vehicle, boolean finance, double loan) {
-
-        return 0;
+        double saleTax = vehicle.getPrice() * 0.05;
+        double recordingFee = 100;
+        double processingFee;
+        double finalprice = 0;
+        if (vehicle.getPrice() <= 10000) {
+            processingFee = 295;
+        } else {
+            processingFee = 495;
+        }
+        if (finance) {
+            finalprice = getMonthlyPayment(vehicle, loan);
+        } else {
+            finalprice = vehicle.getPrice() + saleTax + recordingFee + processingFee;
+        }
+        return finalprice;
     }
 
     @Override
     double getMonthlyPayment(Vehicle vehicle, double loan) {
+        double monthlyCalculation = 0;
+        if (loan < vehicle.getPrice()) {
+            System.out.println("You need to take out a bigger loan!");
+        } else {
+            double annualInterestRate;
+            int months;
+            if (loan >= 10000) {
+                annualInterestRate = 0.0425;
+                months = 48;
+            } else {
+                annualInterestRate = 0.0525;
+                months = 24;
+            }
 
-        return 0;
+            double monthlyRate = annualInterestRate / 12;
+
+            monthlyCalculation = loan * (monthlyRate * Math.pow(1 + monthlyRate, months)) / (Math.pow(1 + monthlyRate, months) - 1);
+        }
+
+        return monthlyCalculation;
     }
 }
